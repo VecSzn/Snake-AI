69f28675ab0d9fd8ab1bb0ebdd8d636eedacc345
VecSzn <82099364+VecSzn@users.noreply.github.com>
2023-11-01 09:33:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import random
 from minigame_framework import MiniGameFramework
 
 #Snake小游戏 贪吃蛇
+class SnakeGame(MiniGameFramework):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.snake = None #Snake Function 还没写
+        self.food = None  #Food Function 还没写
+
+    def handle_events(self):
+        super().handle_events()
+
+    def update(self):
+        pass
+
+    def render(self):
+        self.display.fill((0, 0, 0))
+        pygame.display.update()
